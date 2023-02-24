10053e5f1911e0e63247d1aa02291c2532cc7f36
Kirill <volkill73.me@gmail.com>
2023-02-24 18:21:04 +0300

    version 0.0.3.2

--- pages/footer.php
+++ pages/footer.php
@@ -1,26 +1,28 @@
 <footer>
 	<div class="foot-cont">
 		<div class="soc-block">
-			<div class="soc-cont">
-				<a href="https://www.youtube.com/channel/UCK8vTUV-lfJ7N2IAWstVZXg?view_as=subscriber" target="_blank">
-					<div class="soc-item">
-						<img class="soc" src="/img/yt.svg">
-						<p class="sign">Смотри видео на YouTube</p>
-					</div>
-				</a>
-			</div>	
-			<div class="soc-cont">
-				<a href="https://vk.com/mgok.online" target="_blank">
-					<div class="soc-item">
-						<img class="soc" src="/img/vk.svg">
-						<p class="sign">Заходи в нашу группу ВКонтакте</p>
-					</div>
-				</a>
-			</div>
+			/*
+            *<div class="soc-cont">
+			*	<a href="https://www.youtube.com/channel/UCK8vTUV-lfJ7N2IAWstVZXg?view_as=subscriber" target="_blank">
+			*		<div class="soc-item">
+			*			<img class="soc" src="/img/yt.svg">
+			*			<p class="sign">Смотри видео на YouTube</p>
+			*		</div>
+			*	</a>
+			*</div>
+			*<div class="soc-cont">
+			*	<a href="https://vk.com/mgok.online" target="_blank">
+			*		<div class="soc-item">
+			*			<img class="soc" src="/img/vk.svg">
+			*			<p class="sign">Заходи в нашу группу ВКонтакте</p>
+			*		</div>
+			*	</a>
+			*</div>
+            */
 		</div>
 		<div class="cp-block">
 			<a href="https://mgok.mskobr.ru/" target="_blank">
-				<p class="cprght">© ГБПОУ «Московский государственный<br>образовательный комплекс», 2021</p>
+				<p class="cprght">© ГБПОУ «Московский государственный<br>образовательный комплекс», 2023</p>
 			</a>
 		</div>
     </div>
